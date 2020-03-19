67fe14ed57de5e8b0503578a45c84771c80bd1c9
Aerobird98 <Aerobird98x@gmail.com>
2020-03-19 21:36:29 +0100

    Updates

---
+++ precache-manifest.1689de4c1b4e6ecf44d79d7af15c1016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c339fd4afd97b45af028c1b84ff02351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a77e149eef16d387ff",
+    "url": "/static/css/main.f4c9a774.chunk.css"
+  },
+  {
+    "revision": "999c276aba3d3125e021",
+    "url": "/static/js/2.3bcc7e37.chunk.js"
+  },
+  {
+    "revision": "8ce2385a9ea7439468621fb86ef6a20c",
+    "url": "/static/js/2.3bcc7e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a77e149eef16d387ff",
+    "url": "/static/js/main.44c35b24.chunk.js"
+  },
+  {
+    "revision": "ec7c07544bedb0767698",
+    "url": "/static/js/runtime-main.c1d65fc6.js"
+  }
+]);
