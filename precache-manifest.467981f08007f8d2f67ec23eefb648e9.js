9ff98a1d72d5b67b4b99e57d8a05ee9ded89e11f
Aerobird98 <Aerobird98x@gmail.com>
2020-03-23 12:11:30 +0100

    Updates

---
+++ precache-manifest.467981f08007f8d2f67ec23eefb648e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78764ce40ccec50c3784bf6730070e19",
+    "url": "/flow/index.html"
+  },
+  {
+    "revision": "df1b9b1a779dcae19ae2",
+    "url": "/flow/static/css/main.ab28cf98.chunk.css"
+  },
+  {
+    "revision": "ef70f35a9c976399bc67",
+    "url": "/flow/static/js/2.ea6c1e69.chunk.js"
+  },
+  {
+    "revision": "ebe3e3146ec8811972f3f01e6a145274",
+    "url": "/flow/static/js/2.ea6c1e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1b9b1a779dcae19ae2",
+    "url": "/flow/static/js/main.7102f9fe.chunk.js"
+  },
+  {
+    "revision": "e492d0d775a1872b6412",
+    "url": "/flow/static/js/runtime-main.cbf286c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wordify/precache-manifest.5d505b1e4389a671def49fda40652e08.js"
+  "/flow/precache-manifest.467981f08007f8d2f67ec23eefb648e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wordify/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flow/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
