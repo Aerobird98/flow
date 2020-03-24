a49e479d0038e63503fb52b65eb8d71183beb471
Aerobird98 <Aerobird98x@gmail.com>
2020-03-24 18:38:24 +0100

    added bootstrap for general styling

--- src/index.js
+++ src/index.js
@@ -6,11 +6,25 @@ import { Slate, useSlate, Editable, withReact } from 'slate-react'
 import { withHistory } from 'slate-history'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBold, faItalic, faUnderline, faStrikethrough, faCode, faSubscript, faSuperscript, faHeading, faQuoteLeft, faListOl, faListUl, faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import {
+    faBold,
+    faItalic,
+    faUnderline,
+    faStrikethrough,
+    faCode,
+    faSubscript,
+    faSuperscript,
+    faHeading,
+    faQuoteLeft,
+    faAlignLeft,
+    faAlignCenter,
+    faAlignRight,
+    faAlignJustify
+} from '@fortawesome/free-solid-svg-icons'
 
 import * as serviceWorker from './serviceWorker'
 
-import './index.css'
+import './index.scss';
 
 const Leaf = ({ attributes, children, leaf }) => {
     if (leaf.bold) {
@@ -47,27 +61,21 @@ const Leaf = ({ attributes, children, leaf }) => {
 const Element = ({ attributes, children, element }) => {
     switch (element.type) {
         case 'heading-one':
-            return <h1 {...attributes}>{children}</h1>
+            return <h1 className={element.align} {...attributes}>{children}</h1>
         case 'heading-two':
-            return <h2 {...attributes}>{children}</h2>
+            return <h2 className={element.align} {...attributes}>{children}</h2>
         case 'heading-three':
-            return <h3 {...attributes}>{children}</h3>
+            return <h3 className={element.align} {...attributes}>{children}</h3>
         case 'heading-four':
-            return <h4 {...attributes}>{children}</h4>
+            return <h4 className={element.align} {...attributes}>{children}</h4>
         case 'heading-five':
-            return <h5 {...attributes}>{children}</h5>
+            return <h5 className={element.align} {...attributes}>{children}</h5>
         case 'heading-six':
-            return <h6 {...attributes}>{children}</h6>
+            return <h6 className={element.align} {...attributes}>{children}</h6>
         case 'block-quote':
-            return <blockquote {...attributes}>{children}</blockquote>
-        case 'list-item':
-            return <li {...attributes}><span className='fa-li'><FontAwesomeIcon icon={faLightbulb} /></span>{children}</li>
-        case 'unordered-list':
-            return <ul className='fa-ul' {...attributes}>{children}</ul>
-        case 'ordered-list':
-            return <ol {...attributes}>{children}</ol>
+            return <blockquote className={element.align} {...attributes}>{children}</blockquote>
         default:
-            return <p {...attributes}>{children}</p>
+            return <p className={element.align} {...attributes}>{children}</p>
     }
 }
 
@@ -85,9 +93,16 @@ const FlowEditor = {
         return !!match
     },
 
+    isAlignActive(editor, align) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.align === align,
+        })
+
+        return !!match
+    },
+
     toggleMark(editor, format) {
         const isActive = FlowEditor.isMarkActive(editor, format)
-
         if (isActive) {
             Editor.removeMark(editor, format)
         } else {
@@ -96,44 +111,21 @@ const FlowEditor = {
     },
 
     toggleBlock(editor, format) {
-        if (format === 'list-item') {
-            return
-        }
-
         const isActive = FlowEditor.isBlockActive(editor, format)
-
-        const LIST_TYPES = ['ordered-list', 'unordered-list']
-        const isList = LIST_TYPES.includes(format)
-
-        Transforms.unwrapNodes(editor, {
-            match: n => LIST_TYPES.includes(n.type),
-            split: true,
-        })
-
         Transforms.setNodes(editor,
-            { type: isActive ? null : isList ? 'list-item' : format },
+            { type: isActive ? null : format },
             { match: n => Editor.isBlock(editor, n) }
         )
-
-        if (!isActive && isList) {
-            const block = { type: format, children: [] }
-            Transforms.wrapNodes(editor, block)
-        }
     },
-}
 
-const defaultValue = {
-    type: 'paragraph',
-    children: [
-        {
-            text: 'A line of text in a paragraph.'
-        },
-    ]
+    changeAlign(editor, align) {
+        Transforms.setNodes(editor, { align: align })
+    }
 }
 
 const Flow = () => {
     const editor = useMemo(() => withHistory(withReact(createEditor())), [])
-    const [value, setValue] = useState(JSON.parse(localStorage.getItem('content')) || [defaultValue])
+    const [value, setValue] = useState(JSON.parse(localStorage.getItem('content')) || [])
 
     const renderLeaf = useCallback(props => {
         return <Leaf {...props} />
@@ -153,7 +145,7 @@ const Flow = () => {
                 localStorage.setItem('content', content)
             }}
         >
-            <div className='tools'>
+            <div className='d-print-none sticky-top bg-light'>
                 <MarkButton format='bold' icon={faBold} />
                 <MarkButton format='italic' icon={faItalic} />
                 <MarkButton format='underline' icon={faUnderline} />
@@ -163,41 +155,46 @@ const Flow = () => {
                 <MarkButton format='superscript' icon={faSuperscript} />
                 <BlockButton format='heading-one' icon={faHeading} />
                 <BlockButton format='block-quote' icon={faQuoteLeft} />
-                <BlockButton format='unordered-list' icon={faListUl} />
-                <BlockButton format='ordered-list' icon={faListOl} />
+                <AlignButton format='text-left' icon={faAlignLeft} />
+                <AlignButton format='text-center' icon={faAlignCenter} />
+                <AlignButton format='text-right' icon={faAlignRight} />
+                <AlignButton format='text-justify' icon={faAlignJustify} />
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Storage</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">content</li>
+                    </ol>
+                </nav>
             </div>
-            <Editable
+            <Editable className='p-5'
                 renderLeaf={renderLeaf}
                 renderElement={renderElement}
-                placeholder="Compose something epic..."
                 onKeyDown={event => {
-                    if (!event.ctrlKey) {
-                        return
-                    }
-
-                    switch (event.key) {
-                        case 'b': {
-                            event.preventDefault()
-                            FlowEditor.toggleMark(editor, 'bold')
-                            break
+                    if (event.ctrlKey) {
+                        switch (event.key) {
+                            case 'b': {
+                                event.preventDefault()
+                                FlowEditor.toggleMark(editor, 'bold')
+                                break
+                            }
+                            case 'i': {
+                                event.preventDefault()
+                                FlowEditor.toggleMark(editor, 'italic')
+                                break
+                            }
+                            case 'u': {
+                                event.preventDefault()
+                                FlowEditor.toggleMark(editor, 'underline')
+                                break
+                            }
+                            case '`': {
+                                event.preventDefault()
+                                FlowEditor.toggleMark(editor, 'code')
+                                break
+                            }
+                            default:
+                                break
                         }
-                        case 'i': {
-                            event.preventDefault()
-                            FlowEditor.toggleMark(editor, 'italic')
-                            break
-                        }
-                        case 'u': {
-                            event.preventDefault()
-                            FlowEditor.toggleMark(editor, 'underline')
-                            break
-                        }
-                        case '`': {
-                            event.preventDefault()
-                            FlowEditor.toggleMark(editor, 'code')
-                            break
-                        }
-                        default:
-                            break
                     }
                 }}
             />
@@ -205,31 +202,56 @@ const Flow = () => {
     )
 }
 
-const BlockButton = ({ format, icon }) => {
+const FormatButton = ({ isActive, toggleFormat, format, icon }) => {
     const editor = useSlate()
     return (
-        <span className={'btn' + (FlowEditor.isBlockActive(editor, format) ? ' active' : ' disabled')}
+        <span
+            role='button'
+            className={'m-3 btn' + (isActive(editor, format) ? ' active' : '')}
             onMouseDown={event => {
                 event.preventDefault()
-                FlowEditor.toggleBlock(editor, format)
+                toggleFormat(editor, format)
             }}
         >
-            <FontAwesomeIcon icon={icon} fixedWidth />
+            <FontAwesomeIcon
+                icon={icon}
+                size='lg'
+                fixedWidth
+            />
         </span>
     )
 }
 
-const MarkButton = ({ format, icon }) => {
-    const editor = useSlate()
+const BlockButton = ({ format, icon }) => {
     return (
-        <span className={'btn' + (FlowEditor.isMarkActive(editor, format) ? ' active' : ' disabled')}
-            onMouseDown={event => {
-                event.preventDefault()
-                FlowEditor.toggleMark(editor, format)
-            }}
-        >
-            <FontAwesomeIcon icon={icon} fixedWidth />
-        </span>
+        <FormatButton
+            isActive={FlowEditor.isBlockActive}
+            toggleFormat={FlowEditor.toggleBlock}
+            format={format}
+            icon={icon}
+        />
+    )
+}
+
+const MarkButton = ({ format, icon }) => {
+    return (
+        <FormatButton
+            isActive={FlowEditor.isMarkActive}
+            toggleFormat={FlowEditor.toggleMark}
+            format={format}
+            icon={icon}
+        />
+    )
+}
+
+const AlignButton = ({ format, icon }) => {
+    return (
+        <FormatButton
+            isActive={FlowEditor.isAlignActive}
+            toggleFormat={FlowEditor.changeAlign}
+            format={format}
+            icon={icon}
+        />
     )
 }
 
@@ -237,4 +259,5 @@ ReactDOM.render(<Flow />, document.getElementById('editor'))
 
 // If you want your editor to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
+// See the serviceWorker.js script for details.
 serviceWorker.unregister()
